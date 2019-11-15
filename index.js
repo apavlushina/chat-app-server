2a0c10b74cc3540976d753783da69e917763b903
apavlushina <ssemina7@gmail.com>
2019-11-15 10:26:33 +0100

    can send messages

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const express = require("express");
+const bodyParse = require("body-parser");
+const jsonParser = bodyParse.json();
 const app = express();
+app.use(jsonParser);
 
 const port = 4000;
+
+app.get("/", (req, res, next) => {
+  res.send("Hello");
+});
+
+const messages = [];
+
+app.post("/message", (req, res, next) => {
+  const { message } = req.body;
+  messages.push(message);
+  res.send(message);
+});
+
+app.get("/message", (reg, res, next) => {
+  res.send(messages);
+});
 app.listen(port, () => console.log(`Listening on the ${port}`));
