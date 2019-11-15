b3d3ed64b82294a2ea695999f266a0014fd966de
apavlushina <ssemina7@gmail.com>
2019-11-15 10:38:47 +0100

    have stream

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParse = require("body-parser");
 const jsonParser = bodyParse.json();
+const Sse = require("json-sse");
 const app = express();
 app.use(jsonParser);
 
@@ -8,6 +9,14 @@ const port = 4000;
 
 app.get("/", (req, res, next) => {
   res.send("Hello");
+});
+
+const stream = new Sse();
+
+app.get("/stream", (req, res, next) => {
+  const string = JSON.stringify(messages);
+  stream.updateInit(string);
+  stream.init(req, res);
 });
 
 const messages = [];
@@ -21,4 +30,5 @@ app.post("/message", (req, res, next) => {
 app.get("/message", (reg, res, next) => {
   res.send(messages);
 });
+
 app.listen(port, () => console.log(`Listening on the ${port}`));
